a020c151bc53a27fb3846c74035ebbda28941f36
LaraSakanaka <85855158+LaraYuka@users.noreply.github.com>
2021-09-04 16:35:34 +0000

    Initial commit

---
+++ js/botao.js
@@ -0,0 +1,121 @@
+$(document).ready(function(){ 
+
+  $("a").click(function(e){
+    e.preventDefault()
+
+    let pagina = $(this).attr('href')
+  
+
+    $('.modal-title').empty()
+    $('.modal-body').empty()
+
+    switch(pagina){
+      case 'home':
+        $(location).attr('href', 'index.html');
+      break
+      case 'doce':
+        $('.modal-title').append('Doce')
+        $('.modal-body').append(`
+          <div class="container">
+            <div class="row">
+              <div class="col-12 col-sm-8 col-md-8">
+                <h4 class="text-center text-titulo">Doce</h4>
+                <p>Por enquanto não possui nada nessa página</p>
+              </div>
+            </div>
+          </div>
+        `)
+      break
+      case 'salgado':
+        $('.modal-title').append('Salgado')
+        $('.modal-body').append(`
+          <div class="container">
+            <div class="row">
+              <div class="col-12 col-sm-8 col-md-8">
+                <h4 class="text-center text-titulo">Salgado</h4>
+                <p>Por enquanto não possui nada nessa página</p>
+              </div>
+            </div>
+          </div>
+        `)
+      break
+      case 'contato':
+        $('.modal-title').append('Contato')
+        $('.modal-body').append(`
+          <div class="container">
+            <div class="row">
+              <div class="col-12 col-sm-8 col-md-8">
+                <h4 class="text-center text-titulo">Contato</h4>
+                <p>Por enquanto não possui nada nessa página</p>
+              </div>
+            </div>
+          </div>
+        `)
+      break
+      case 'cadastro':
+        $('.modal-title').append('Cadastro')
+        $('.modal-body').append(`
+          <div class="container">
+            <div class="row">
+              <div class="col-12 col-sm-8 col-md-8">
+                <h4 class="text-center text-titulo">Cadastro</h4>
+                <p>Por enquanto não possui nada nessa página</p>
+              </div>
+            </div>
+          </div>
+        `)
+      break
+      case 'user':
+        $('.modal-title').append('User')
+        $('.modal-body').append(`
+          <div class="container">
+            <div class="row">
+              <div class="col-12 col-sm-8 col-md-8">
+                <h4 class="text-center text-titulo">User</h4>
+                <p>Por enquanto não possui nada nessa página</p>
+              </div>
+            </div>
+          </div>
+        `)
+      break
+      case 'receita':
+        $('.modal-title').append('Receita')
+        $('.modal-body').append(`
+          <div class="container">
+            <div class="row">
+              <div class="col-12 col-sm-8 col-md-8">
+                <h4 class="text-center text-titulo">Receita</h4>
+                <p>Por enquanto não possui nada nessa página</p>
+              </div>
+            </div>
+          </div>
+        `)
+      break
+      case 'whats':
+        $('.modal-title').append('Whatsapp')
+        $('.modal-body').append(`
+          <div class="container">
+            <div class="row">
+              <div class="col-12 col-sm-8 col-md-8">
+                <h4 class="text-center text-titulo">Número</h4>
+                <p>(XX) XXXXXXXXX</p>
+              </div>
+            </div>
+          </div>
+        `)
+      break
+      case 'insta':
+        $(location).attr('href', 'https://www.instagram.com/larasakanaka/');
+      break
+      case 'face':
+        $(location).attr('href', 'https://www.facebook.com/lara.sakanaka.9');
+      break
+      default: alert('Página não encontrada')
+    }
+
+
+    $('#exampleModal"').modal('show');
+    
+
+  })
+})
