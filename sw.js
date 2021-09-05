0138986c902baa7ae58b473dd528104c1feae722
Lara Sakanaka <85855158+LaraYuka@users.noreply.github.com>
2021-09-05 00:34:57 +0000

    adição do sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,20 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('./sw.js').then(function(registration) {
-      // Registration was successful
-      console.log('O registro do ServiceWorker foi bem-sucedido com o escopo: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('Falha no registro do ServiceWorker: ', err);
-    });
-  });
-}
+var cacheName = 'NomeApp-v';
+var filesToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache){
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response){
+    return response || fetch(e.request);
+    })
+  );
+});
